f121381dc5c397cf1d6c4175a1128413ffcc0ac3
Darian Smith <meep3692@gmail.com>
2024-11-19 11:21:57 -0500

    Fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/aliens.omp.json | source
+
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
