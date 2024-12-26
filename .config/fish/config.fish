968bc1e26778e2355aa7945da622699a0cfc5210
Darian Smith <meep3692@gmail.com>
2024-12-26 05:02:33 -0500

    fix guix locale

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ end
 oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/aliens.omp.json | source
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+    export GUIX_LOCPATH="$HOME/.guix-profile/lib/locale"
